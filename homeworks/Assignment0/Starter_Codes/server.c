2ed88395a9bccd850b03ecd1fb775792b34036e7
halbert <cburhoe1@jhu.edu>
2025-01-26 12:42:09 -0500

    Skeleton for server() up to and including accept()

--- homeworks/Assignment0/Starter_Codes/server.c
+++ homeworks/Assignment0/Starter_Codes/server.c
@@ -18,7 +18,30 @@
  * Return 0 on success, non-zero on failure
 */
 int server(char *server_port) {
-    return 0;
+  struct sockaddr_storage their_addr;
+  socklen_t addr_size;
+  struct addrinfo hints, *res;
+  int sockfd, new_fd;
+
+  // TODO: include error handling for above
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+
+  getaddrinfo(NULL, server_port, &hints, &res);
+
+  sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+  bind(sockfd, res->ai_addr, res->ai_addrlen);
+  listen(sockfd, QUEUE_LENGTH);
+
+  addr_size = sizeof their_addr;
+  new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+
+  //TODO: handle communication on new_fd
+
+  return 0;
 }
 
 /*

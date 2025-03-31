8e11ed1245ac3d7ad8ac904caace78380144182a
halbert <cburhoe1@jhu.edu>
2025-03-30 20:41:05 -0400

    back to FF

--- homeworks/Assignment2/src/router/sr_router.c
+++ homeworks/Assignment2/src/router/sr_router.c
@@ -342,7 +342,7 @@ void send_arpreq(struct sr_instance* sr,
   arp_reply_arp_hdr->ar_op = htons(arp_op_request);
   memcpy(arp_reply_arp_hdr->ar_sha, iface->addr, ETHER_ADDR_LEN);
   arp_reply_arp_hdr->ar_sip = iface->ip;
-  memset(arp_reply_arp_hdr->ar_tha, 0x00, ETHER_ADDR_LEN);
+  memset(arp_reply_arp_hdr->ar_tha, 0xFF, ETHER_ADDR_LEN);
   arp_reply_arp_hdr->ar_tip = request->ip;
 
 //  arp_hdr_to_network(arp_reply_arp_hdr);

617fda69a41d870291c8a88ca3c12b48b25ba651
halbert <cburhoe1@jhu.edu>
2025-03-30 22:28:29 -0400

    try different size

--- homeworks/Assignment2/src/router/sr_router.c
+++ homeworks/Assignment2/src/router/sr_router.c
@@ -295,7 +295,8 @@ void send_icmp_packet(struct sr_instance* sr,
 
   //Set the IP header fields
   memcpy(new_packet_ip_hdr, packet_ip_hdr, sizeof(sr_ip_hdr_t));
-  new_packet_ip_hdr->ip_len = len - sizeof(sr_ethernet_hdr_t);
+//  new_packet_ip_hdr->ip_len = len - sizeof(sr_ethernet_hdr_t);
+  new_packet_ip_hdr->ip_len = sizeof(sr_ip_hdr_t) + sizeof(sr_icmp_hdr_t);
   if (type != 0) {
     new_packet_ip_hdr->ip_off = IP_DF;
   } else {

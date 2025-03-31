c8c2bd85468d04566164892936ec81b4ce516c96
halbert <cburhoe1@jhu.edu>
2025-03-30 20:37:36 -0400

    try 00 tha

--- homeworks/Assignment2/src/router/sr_router.c
+++ homeworks/Assignment2/src/router/sr_router.c
@@ -330,7 +330,7 @@ void send_arpreq(struct sr_instance* sr,
 
   struct sr_if *iface = sr_get_interface(sr, request->packets->iface);
   //Set the ethernet header fields
-  memset(arp_reply_eth_hdr->ether_dhost, 0xFF, ETHER_ADDR_LEN);
+  memset(arp_reply_eth_hdr->ether_dhost, 0x00, ETHER_ADDR_LEN);
   memcpy(arp_reply_eth_hdr->ether_shost, iface->addr, ETHER_ADDR_LEN);
   arp_reply_eth_hdr->ether_type = htons(ethertype_arp);
 

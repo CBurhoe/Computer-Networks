06006ca48a515d1472a46e24ae36b24eef7568bb
halbert <cburhoe1@jhu.edu>
2025-03-30 21:07:47 -0400

    use sender hardware address as sending mac address

--- homeworks/Assignment2/src/router/sr_router.c
+++ homeworks/Assignment2/src/router/sr_router.c
@@ -128,7 +128,7 @@ void sr_handlepacket(struct sr_instance* sr,
           struct sr_packet *queued_packet = request_queue->packets;
           while (queued_packet) {
             struct sr_ethernet_hdr *queued_packet_eth_hdr = (struct sr_ethernet_hdr *)queued_packet->buf;
-            memcpy(queued_packet_eth_hdr->ether_dhost, sr_get_interface(sr, interface)->addr, ETHER_ADDR_LEN);
+            memcpy(queued_packet_eth_hdr->ether_dhost, packet_arp_hdr->ar_sha, ETHER_ADDR_LEN);
             print_hdrs(queued_packet->buf, queued_packet->len);
             sr_send_packet(sr, queued_packet->buf, queued_packet->len, queued_packet->iface);
             queued_packet = queued_packet->next;
